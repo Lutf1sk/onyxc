d8434d89a655648e6862065da3565c4af1e85190
Alex Edin <lutfisk@lutfisk.net>
2021-10-21 22:10:17 +0200

    Give test output fancier formatting

--- tests/runall.sh
+++ tests/runall.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
+failed=0
+succeeded=0
+
 for D in tests/*; do
     if [ -d "${D}" ]; then
-        sh $D/run.sh $D
+		info=$(cat $D/info.txt)
+        output=$(sh $D/run.sh $D)
 		if [ $? == 0 ] ; then
-			echo -e "$(cat $D/info.txt) \x1b[32mSucceeded\x1b[0m"
+			printf "%-24s \x1b[32m%-12s\x1b[0m $output\n" "$info" "Succeeded"
+			succeeded=$((succeeded+1))
 		else
-			echo -e "$(cat $D/info.txt) \x1b[31mFailed\x1b[0m"
+			printf "%-24s \x1b[31m%-12s\x1b[0m $output\n" "$info" "Failed"
+			failed=$((failed+1))
 		fi
 		
     fi
 done
+
+total=$((succeeded+failed))
+printf "($succeeded/$total) Tests passed\n"
 
